d1e8146ba8eade3023281079e40716436e1d7d24
Allumodex <spm11347@gmail.com>
2024-12-25 11:06:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
-PREFIX: process.env.PREFIX || ".",
+SESSION_ID: process.env.SESSION_ID || "HrRmwQSS#QOULNHdKJHnyxoVGy-iptqLJfiylI1cIj-1-Q281V6Q", // Add your session id
+PREFIX: process.env.PREFIX || "!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
